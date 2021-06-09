4cbc0c96d59fce3cfad365c731715582e08f8043
kirill89450 <49889375+kirill89450@users.noreply.github.com>
2021-06-09 22:31:56 +0700

    добавила страницу с кредитом
    только правая часть с левой работаем для переключения в 1 окне

---
+++ sample/crediteController.java
@@ -0,0 +1,103 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+public class crediteController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Slider slide1;
+    int myRub;
+
+    @FXML
+    private TextField mytextField;
+
+    @FXML
+    private Button credit_btn;
+
+    @FXML
+    private Slider slide2;
+    int age;
+
+    @FXML
+    private TextField mytextField1;
+
+    @FXML
+    private TextField mytextField2;
+
+    @FXML
+    private TextField mytextField_age;
+
+    @FXML
+    public void handlekeyPressed(KeyEvent KE) {
+        if (KE.getCode() == KeyCode.ENTER){
+            myRub=Integer.parseInt(mytextField.getText());
+            slide1.setValue(myRub);
+            mytextField.end();
+        }
+    }
+    @FXML
+    public void handlekeyPressed2(KeyEvent KE) {
+        if (KE.getCode() == KeyCode.ENTER){
+            age=Integer.parseInt(mytextField_age.getText());
+            slide2.setValue(age);
+            mytextField_age.end();
+        }
+    }
+
+    @FXML
+    void initialize() {
+
+
+        slide1.valueProperty().addListener(new ChangeListener<Number>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+
+                myRub= (int) slide1.getValue();
+                mytextField.setText(Integer.toString(myRub));
+                if (age == 0){
+                    mytextField1.setText(Integer.toString(0));
+                }
+                else {
+                    mytextField1.setText(Integer.toString((int) (myRub*1.05/age/12)));
+                }
+            }
+        });
+
+        slide2.valueProperty().addListener(new ChangeListener<Number>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+
+                age= (int) slide2.getValue();
+                mytextField_age.setText(Integer.toString(age));
+                if (age == 0){
+                    mytextField1.setText(Integer.toString(0));
+                }
+                else {
+                    mytextField1.setText(Integer.toString((int) (myRub*1.05/age/12)));
+                }
+
+
+            }
+        });
+
+
+    }
+}
