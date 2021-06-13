c0973ed8761c91db12bf0b8d9684ff6094b6e76b
Takeitez17 <m.roganez@gmail.com>
2021-06-13 19:42:36 +0700

    Обновил вкладку "Взять кредит" и связал с главным меню

--- sample/crediteController.java
+++ sample/crediteController.java
@@ -1,16 +1,22 @@
 package sample;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 
 public class crediteController {
 
@@ -31,6 +37,9 @@ public class crediteController {
     private Button credit_btn;
 
     @FXML
+    private Button back;
+
+    @FXML
     private Slider slide2;
     int age;
 
@@ -42,6 +51,20 @@ public class crediteController {
 
     @FXML
     private TextField mytextField_age;
+
+    @FXML
+    void Back(ActionEvent event) throws IOException {
+        Stage stage = (Stage) back.getScene().getWindow();
+        stage.close();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("General.fxml"));
+        Parent root = (Parent) loader.load();
+        stage = new Stage();
+        stage.setTitle("Bank System");
+        stage.setScene(new Scene(root,906,540));
+        stage.setResizable(false);
+        stage.show();
+
+    }
 
     @FXML
     public void handlekeyPressed(KeyEvent KE) {
